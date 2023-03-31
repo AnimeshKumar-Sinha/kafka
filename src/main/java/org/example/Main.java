0667fe2b7d52fdd8cce2478997863d62502d1385
animeshsinha <animesh7979@gmail.com>
2023-03-31 00:04:10 -0400

    src/main/java/org/example/Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! YES");
+        System.out.println("Hello world! YES NOgit s");
     }
 }
