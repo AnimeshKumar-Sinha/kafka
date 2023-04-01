6d55152bdc3c1cecaf1122c4c41ff13ea96c8d9c
animeshsinha <animesh7979@gmail.com>
2023-04-01 11:36:40 -0400

    modified

---
+++ src/main/java/com/kafka/Main.java
@@ -0,0 +1,18 @@
+package com.kafka;
+
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! YES NO");
+
+        // Create Producer Properties
+        Properties karkaprop = new Properties();
+
+
+        //Create Producer
+
+        //Send Data
+
+    }
+}
